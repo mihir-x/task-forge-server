7f6ce0fdd9339944d611020a98446c489072edb9
mihir-x <ictlancerx@gmail.com>
2023-12-21 23:35:05 +0600

    added apis to get individual task and update task

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
 
@@ -88,9 +88,34 @@ async function run() {
         })
 
         //task related api---------------------------------------------------
+        app.get('/task/:email', verifyToken, async(req, res)=>{
+            const email = req.params.email
+            const query = {creator: email}
+            const result = await taskCollection.find(query).toArray()
+            res.send(result)
+        })
+        app.get('/task/edit/:id', verifyToken, async(req, res) =>{
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await taskCollection.findOne(query)
+            res.send(result)
+        })
         app.post('/task', verifyToken, async(req, res)=>{
             const data = req.body
             const result = await taskCollection.insertOne(data)
+            res.send(result)
+        })
+        app.put('/task/update/:id', verifyToken, async(req, res)=>{
+            const id = req.params.id
+            const task = req.body
+            const query = {_id: new ObjectId(id)}
+            const options = {upsert: true}
+            const updateDoc = {
+                $set:{
+                    ...task
+                }
+            }
+            const result = await taskCollection.updateOne(query, updateDoc, options)
             res.send(result)
         })
 
